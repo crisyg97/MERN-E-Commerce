bb61f9a547cdf87bd20e4c49f34da146a712c96c
crisyg97 <cristiangaray101@gmail.com>
2021-07-24 21:30:39 -0300

    fix product controller and routes

--- Backend/controllers/product.js
+++ Backend/controllers/product.js
@@ -4,7 +4,7 @@ const ctrl = {}
 ctrl.index = async (req, res, next) => {
     await productModel.find((err,product) => {
         if(err) {console.log(err)}
-        res.send({
+        res.status(200).json({
             product : product
         });       
     });
@@ -24,26 +24,36 @@ ctrl.create = async (req, res, next) => {
         reviews: body.reviews,
         createdAt: body.createdAt
     });
-    const product = await productModel.create(newProduct); //create and save model
+    newProduct.save((err,product) => {
+        if(err) {res.status(400).json(err)}
+        if(product) {res.status(200).json(product)}
+    })
 }
 
 ctrl.getById = async (req, res, next) => {
-    const id = req.params.product_id;;
-    await productModel.findById({_id:id}, (productDb,err) => {
+    const id = req.params.product_id;
+    await productModel.findById({_id:id}, (err,productDb) => {
+        console.log("product found: ", productDb);
         if(err) {console.log(err)}
-        res.send({
-            product: productDb
-        });
-    })
+        if(!productDb) {
+            res.status(400).json({
+                message: 'product not found'
+            });
+        }else{
+            res.status(200).json({
+                product: productDb
+            });
+        }
+    });
 }
 
 ctrl.update = async (req, res, next) => {
     const id = req.params.product_id;
     const body = req.body;
-    await productModel.findOne({_id:id}, (productDb,err) => {
+    await productModel.findOne({_id:id}, (err, productDb) => {
         if(err) {console.log(err)}
         else
-            if(!user) {console.log('product not found')}
+            if(!productDb) {res.status(400).json({ message: 'product not found'})}
             else{
                 productDb.name= body.name,
                 productDb.price= body.price,
@@ -57,8 +67,8 @@ ctrl.update = async (req, res, next) => {
                 productDb.createdAt= body.createdAt
             }
             productDb.save((err) => {
-                if(err) {console.log(err)}
-                res.send({
+                if(err) {return res.status(400).json({ message: 'something went wrong'})}
+                res.status(200).json({
                     succes: true
                 });
             })
@@ -68,16 +78,17 @@ ctrl.update = async (req, res, next) => {
 
 ctrl.remove = async (req, res, next) => {
     const id = req.params.product_id;
-    await productModel.findOne({_id:id}, (productDb,err) => {
-        if(err) {console.log(err)}
+    await productModel.findOne({_id:id}, (err,productDb) => {
+        if(err) {
+            res.status(400).json({ message: 'something went wrong'})}
         else
-            if(!productDb) {console.log('product not found')}
+            if(!productDb) {res.status(400).json({message: 'product not found'})}
             else{
                 productDb.status = 'INACTIVE';
                 
                 productDb.save((err) => {
-                    if(err) {console.log(err)}
-                    res.send({
+                    if(err) {res.status(400).json({ message: 'something went wrong'})}
+                    res.status(200).json({
                         success: true
                     });
                 })
--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -6,7 +6,7 @@ const product = require('../controllers/product');
 router.get('/', product.index );
 router.post('/add', product.create);
 router.get('/:product_id', product.getById);
-router.post('/:product_id/update', product.update);
+router.put('/:product_id/update', product.update);
 router.post('/:product_id/delete', product.remove);
 
 module.exports = router;
