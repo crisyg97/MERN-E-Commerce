2425367019076eb2e982e828e1b0aae06ef3eb30
crisyg97 <cristiangaray101@gmail.com>
2021-08-03 20:26:59 -0300

    fixed createDefaultRoles, error in exec the consult

--- Backend/utils/createDefaultRole.js
+++ Backend/utils/createDefaultRole.js
@@ -7,17 +7,20 @@ dotenv.config({ path: 'backend/config/config.env' });
 connectionDb.connect();
 
 const createRoles =  async () => {
+    var values;
     try{
         //check for existing roles
         const count =  await roleModel.estimatedDocumentCount();
         console.log(count);
-        if(count > 0) return;
-        const values =  Promise.all ([
-            new roleModel({name: "user"}).save(),
-            new roleModel({name: "admin"}).save(),
-            new roleModel({name: "moderator"}).save()
-        ]);
-        console.log('roles Created');
+        if(count > 0) {
+            return process.exit();
+        }else {
+            values =  await Promise.all ([
+                new roleModel({name: "user"}).save(),
+                new roleModel({name: "admin"}).save(),
+                new roleModel({name: "moderator"}).save()
+            ]).then((res) => {console.log('roles Created')});
+        }
         process.exit();
     }catch(err){
         console.log(err);
